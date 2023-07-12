ffa30f95565db9394b26175ddc4ddb64f8ea45f0
Milos <milonite@hotmail.it>
2023-07-12 21:20:21 +0200

    structure

--- theme.config.tsx
+++ theme.config.tsx
@@ -18,12 +18,12 @@ const config: DocsThemeConfig = {
     next: true,
   },
   project: {
-    link: "https://github.com/shuding/nextra-docs-template",
+    link: "https://github.com/EnsoFinance/shortcuts-docs",
   },
   chat: {
     link: "https://discord.com/invite/enso-finance",
   },
-  docsRepositoryBase: "https://github.com/shuding/nextra-docs-template",
+  docsRepositoryBase: "https://github.com/EnsoFinance/shortcuts-docs",
   footer: {
     text: "Enso Shortcuts Registry Docs",
   },
