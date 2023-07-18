c69574cb7c0af47aacf0650cfebfb9505317f2d2
Milos <milonite@hotmail.it>
2023-07-18 16:39:08 +0200

    logo colors, examples smart wallet (#16)

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,16 +1,14 @@
-import { DocsThemeConfig } from "nextra-theme-docs";
-import { useConfig } from "nextra-theme-docs";
-import { useRouter } from "next/router";
-import Image from "next/image";
-import logo from "./assets/logow.png";
-import React from "react";
+import { DocsThemeConfig } from 'nextra-theme-docs'
+import { useConfig } from 'nextra-theme-docs'
+import { useRouter } from 'next/router'
+import Image from 'next/image'
+import logo from './assets/logo_horizontal.png'
+import React from 'react'
 
 const config: DocsThemeConfig = {
   logo: (
     <>
-      <Image src={logo} alt="Enso Logo" width="32" height="32" />
-
-      <span style={{ marginLeft: ".4em", fontWeight: 600 }}>Enso API Docs</span>
+      <Image src={logo} alt="Enso Logo" width="148" height="64" />
     </>
   ),
   navigation: {
@@ -18,27 +16,27 @@ const config: DocsThemeConfig = {
     next: true,
   },
   project: {
-    link: "https://enso.finance",
+    link: 'https://enso.finance',
   },
   chat: {
-    link: "https://t.me/enso_api",
+    link: 'https://t.me/enso_api',
   },
-  docsRepositoryBase: "https://github.com/EnsoFinance/shortcuts-docs",
+  docsRepositoryBase: 'https://github.com/EnsoFinance/shortcuts-docs',
   footer: {
-    text: "Enso Shortcuts Registry Docs",
+    text: 'Enso Shortcuts Registry Docs',
   },
   useNextSeoProps() {
-    const { route } = useRouter();
-    if (route !== "/") {
+    const { route } = useRouter()
+    if (route !== '/') {
       return {
-        titleTemplate: "%s – Enso Docs",
-      };
+        titleTemplate: '%s – Enso Docs',
+      }
     }
   },
 
   head: () => {
-    const { asPath } = useRouter();
-    const { frontMatter } = useConfig();
+    const { asPath } = useRouter()
+    const { frontMatter } = useConfig()
     return (
       <>
         <meta
@@ -47,17 +45,17 @@ const config: DocsThemeConfig = {
         />
         <meta
           property="og:title"
-          content={frontMatter.title || "Enso Shortcuts API"}
+          content={frontMatter.title || 'Enso Shortcuts API'}
         />
         <meta
           property="og:description"
           content={
-            frontMatter.description || "The Enso Shortcuts API documentation"
+            frontMatter.description || 'The Enso Shortcuts API documentation'
           }
         />
       </>
-    );
+    )
   },
-};
+}
 
-export default config;
+export default config
