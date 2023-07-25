a26a0d56d9e6ff98c71d254a959cd16fcf630cd8
Milos <milonite@hotmail.it>
2023-07-25 17:48:20 +0200

    hot fix search

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
-const withNextra = require('nextra')({
-  theme: 'nextra-theme-docs',
-  themeConfig: './theme.config.tsx',
-})
+const withNextra = require("nextra")({
+  theme: "nextra-theme-docs",
+  themeConfig: "./theme.config.tsx",
+  flexsearch: {
+    codeblocks: false,
+  },
+});
 
-module.exports = withNextra()
+module.exports = withNextra();
